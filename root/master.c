3c72da0a30bf510455306ef625650034d6db2d2e
TomasBalboa <tbalboakoolen@itba.edu.ar>
2024-08-29 00:13:35 -0300

    Creacion de Esclavos, Pipes y Func Auxs

--- root/master.c
+++ root/master.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errorCode.h>
+#include <master.h>
 
 #define DIV 10  //porque queremos hacer 10% de esclavos
+
+void setPipes(int i, Pipes *allPipes); 
+void runSlave(); 
 
 int main(int argc, char *argv[]){
     int slavesQty = (argc-1)/DIV;
@@ -16,21 +20,33 @@ int main(int argc, char *argv[]){
         }
 
         pid_t pid = fork();
-        //char *args[] = {NULL};
-        //char *envp[] = {NULL}; LOS PUSE DIRECTO EN EL EXECVE
 
         if(pid==0){
-            dup2(pipes[i].pipefd1[0], STDIN_FILENO); //redirigimos la entrada estandar del slave al pipe de lectura
-            dup2(pipes[i].pipefd2[1], STDOUT_FILENO); //redirigimos la salida estandar del slave al pipe de escritura
-
-            close(pipes[i].pipefd1[0]); //cerramos el pipe de lectura del padre
-            close(pipes[i].pipefd2[1]);
-
-            execve("./slave", char *args[] = {NULL}, char *envp[] = {NULL}); //ejecutamos el slave y los parametros son null porque usamos pipe
-            perror("[master] slave execute fail"); //por si execve falla
-            exit(ERROR_SLAVE_EXECVE);
+            setPipes(i, allPipes); //seteo los pipes
+            runSlave(); //ejecuto el slave
         }
 
     }
+}
+
+void setPipes(int i, Pipes *allPipes){
+    close(allPipes[i].masterToSlave[1]); //cierro el pipe de escritura del master | Desde el hijo no voy a escribir nada en el masterToSlave    
+    close(allPipes[i].slaveToMaster[0]); //cierro el pipe de lectura del master | Desde el hijo no voy a leer nada del slaveToMaster
+
+    dup2(allPipes[i].masterToSlave[0], STDIN_FILENO); //Estoy mandando el pipe a la entrada estandar del slave. 
+    close(allPipes[i].masterToSlave[0]); //cierro el pipe de lectura del master | Ya no lo necesito
+
+    dup2(allPipes[i].slaveToMaster[1], STDOUT_FILENO); //Estoy mandando la salida estandar del slave al pipe de escritura. 
+    close(allPipes[i].slaveToMaster[1]); //cierro el pipe de escritura del master | Ya no lo necesito
+}
+
+void runSlave(){
+    char *args[] = { "./slave", NULL };
+    char *envp[] = { NULL };
+
+    execve(args[0], args, envp); //ejecutamos el slave y los parametros son null porque usamos pipe
+
+    perror("[master] slave execute fail"); //por si execve falla
+    exit(ERROR_SLAVE_EXECVE);
 }
 
--- root/slave.c
+++ root/slave.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 256
 
